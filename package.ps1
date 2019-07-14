386c7e4949e68a73dcb0d247041ec050a15d7311
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2019-07-14 20:36:49 +0100

    Add appveyor to upload package

---
+++ package.ps1
@@ -0,0 +1,37 @@
+#!/bin/pwsh
+$ErrorActionPreference = "Stop"
+
+# Create zip archive containing replays
+$useZip = $false
+if (Get-Command "zip" -ErrorAction SilentlyContinue)
+{
+    # Use zip if possible as it handles permissions better on unix
+    $useZip = $true
+    Write-Host "Using zip instead of Compress-Archive"
+}
+
+Write-Host -ForegroundColor Cyan "Re-creating artifacts directory..."
+Remove-Item -Force -Recurse artifacts -ErrorAction SilentlyContinue
+New-Item -Force -ItemType Directory artifacts,artifacts/replays | Out-Null
+
+Write-Host -ForegroundColor Cyan "Copying replays..."
+Push-Location replays
+    Copy-Item -Recurse ../replays ../artifacts/replays
+Pop-Location
+
+Write-Host -ForegroundColor Cyan "Creating final archive..."
+if ($useZip)
+{
+    Push-Location "artifacts/replays"
+        zip -r9 "../replays.zip" (Get-ChildItem).Name
+        if ($LASTEXITCODE -ne 0)
+        {
+            throw "zip failed with $LASTEXITCODE"
+        }
+    Pop-Location
+}
+else
+{
+    Compress-Archive -Force "artifacts/replays/*" -DestinationPath "artifacts/replays.zip" -CompressionLevel Optimal
+}
+Remove-Item -Force -Recurse artifacts/replays
